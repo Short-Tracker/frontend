143409f738d9c6d44393ae799be9b8d6158d3552
udartapkom <karakoom@ya.ru>
2023-12-21 18:24:29 +0300

    fix any problems

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Main.module.scss';
+
+const Main = () => {
+  return (
+    <main className={styles.main}>
+      <h1>главная страница</h1>
+    </main>
+  );
+};
+
+export default Main;
--- src/types/apiError.ts
+++
@@ -1,3 +0,0 @@
-export interface IloginError {
-  errors: string[] | string;
-}

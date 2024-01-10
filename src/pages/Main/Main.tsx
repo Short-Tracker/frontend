6be6729a1c75bb8b369d6f6e417e0326c4d4f5de
udartapkom <karakoom@ya.ru>
2024-01-10 18:24:07 +0300

    feat Universal button

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { UniversalButton } from 'ui-lib/Buttons';
 import styles from './Main.module.scss';
 
 const Main = () => {
   return (
     <main className={styles.main}>
       <h1>главная страница</h1>
+      <UniversalButton>Кнопка</UniversalButton>
     </main>
   );
 };
